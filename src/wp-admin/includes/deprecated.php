536f1a7fd4909787803c990c581ecb6a8fcfd18c
Priyadarshini Kumari <57265762+pawki07@users.noreply.github.com>
2021-01-26 11:26:27 +0530

    Update deprecated.php

--- src/wp-admin/includes/deprecated.php
+++ src/wp-admin/includes/deprecated.php
@@ -76,9 +76,6 @@ function get_udims( $width, $height ) {
  * @see wp_category_checklist()
  *
  * @global int $post_ID
- * @param int $default       Unused.
- * @param int $parent        Unused.
- * @param array $popular_ids Unused.
  */
 function dropdown_categories( $default = 0, $parent = 0, $popular_ids = array() ) {
 	_deprecated_function( __FUNCTION__, '2.6.0', 'wp_category_checklist()' );
